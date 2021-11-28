b01fc4786859749a55bdc0d38f72ae97df2d6eab
Natascha <na7a5cha010@gmail.com>
2021-11-27 21:20:02 -0400

    Gen transactions (#2)
    * Squashed commit of the following:
    
    commit 87ef048adeb9daf807ec4cfd68e1b6beea87e562
    Author: Natascha <na7a5cha010@gmail.com>
    Date:   Fri Nov 26 13:12:47 2021 -0400
    
        Gen identities (#1)
    
        * Squashed commit of the following:
    
        commit 3d847824a4184c3c575b74389a30cdf80624ee9e
        Author: Naty-S <na7a5cha010@gmail.com>
        Date:   Fri Nov 26 11:21:42 2021 -0400
    
            skeleton
    
        commit 4abff324c911805420fa2d4971d7b895bcfe2f77
        Author: Naty-S <na7a5cha010@gmail.com>
        Date:   Fri Nov 26 11:21:03 2021 -0400
    
            project structure
    
        * gen identities as objects
    
    commit 3d847824a4184c3c575b74389a30cdf80624ee9e
    Author: Naty-S <na7a5cha010@gmail.com>
    Date:   Fri Nov 26 11:21:42 2021 -0400
    
        skeleton
    
    commit 4abff324c911805420fa2d4971d7b895bcfe2f77
    Author: Naty-S <na7a5cha010@gmail.com>
    Date:   Fri Nov 26 11:21:03 2021 -0400
    
        project structure
    
    * add global variables file
    
    * rename fiel structure
    
    * generate transacctions without fee

---
+++ modules/identity.py
@@ -0,0 +1,24 @@
+
+
+class Identity:
+
+  def __init__(self, name: str, privKey: str, publKey: str, address: str) -> None:
+    self.name    = name
+    self.privKey = privKey
+    self.publKey = publKey
+    self.address = address
+
+
+class User(Identity):
+
+  def __init__(self, name: str, mail: str, privKey: str, publKey: str, address: str) -> None:
+    super().__init__(name, privKey, publKey, address)
+    self.mail  = mail
+    self.utxos = set()
+    
+
+class Nodo(Identity):
+
+  def __init__(self, name: str, privKey: str, publKey: str, address: str, port: int) -> None:
+    super().__init__(name, privKey, publKey, address)
+    self.port = port
---
+++ modules/merkle.py
@@ -0,0 +1,6 @@
+
+
+class Merkle:
+
+  def __init__(self) -> None:
+    pass
