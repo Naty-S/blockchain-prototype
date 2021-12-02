925bd970a7638d757b43c8f13e21f4a520c545a9
Naty-S <na7a5cha010@gmail.com>
2021-12-02 00:09:35 -0400

    create blockchain structure and send to nodes

--- blockchainSimulator.py
+++ blockchainSimulator.py
@@ -1,14 +1,31 @@
+import os, subprocess, threading
 
-import src.genIdenti   as genIds
-import src.genTransac  as genTxs
-import src.genNet      as genNet
-import src.nodo        as nodo
+import config.variables    as vars
+import modules.blockchain  as blockchain
+import modules.block       as block
+import src.genIdenti       as genIds
+import src.genTransac      as genTxs
+import src.genNet          as genNet
+import src.nodo            as nodo
 
-def blockchainSimulator(i: int = 5, n: int = 3, m: int = 4, d: str = "") -> None:
+def blockchainSimulator(i: int = 5, n: int = 1, m: int = 0, outDir: str = "./output/logs/") -> None:
+  
+  if not os.path.exists(outDir): os.makedirs(outDir)
   
   (users, nodes) = genIds.genIdentities(i, n)
   net = genNet.genNetwork(list(nodes.values()), m)
-  nodo.nodo("nodo0", "", nodes, net)
-  nodo.nodo("nodo1", "", nodes, net)
-  nodo.nodo("nodo2", "", nodes, net)
-  genTxs.genTransactions(users, list(nodes.values()), "./output/logs/")
+
+  # for x in range(n):
+  #   name = 'nodo'+str(x)
+  #   cmd = f"cd 'c:/Users/User/USB/blockchain-prototype'; nodo.nodo({name}, '', {nodes}, {net}, outDir)"
+  #   subprocess.run(["cmd.exe", "/c", "start", f"{cmd}"])
+  bc = blockchain.Blockchain()
+  nodo.nodo("nodo0", nodes, net, outDir, bc)
+  # nodo.nodo("nodo1", nodes, net, outDir, genesis)
+  # nodo.nodo("nodo2", nodes, net, outDir, genesis)
+  # blockchain.Blockchain(vars.DIFFICULTY, list(nodes.values())) # Creates genesis block and spread to network
+  # print(f"threads: {threading.enumerate()}")
+  genTxs.genTransactions(users, list(nodes.values()), outDir)
+
+
+blockchainSimulator()
--- modules/blockchain.py
+++ modules/blockchain.py
@@ -1,5 +1,28 @@
 
+import modules.block       as block
+import modules.transaction as tx
+
+
 class Blockchain:
   
   def __init__(self) -> None:
-    self.chain = []
+    self.__chain = []
+    self.__genGensisBlock()
+
+
+  def __genGensisBlock(self) -> None:
+    pass
+
+
+  def getChain(self) -> list: return self.__chain
+
+
+  def add(self, b: block.Block) -> None: self.__chain.append(b)
+
+
+  def blockExplorer(self) -> block.Block:
+    pass
+
+
+  def txExplorer(self) -> tx.Transaction:
+    pass
