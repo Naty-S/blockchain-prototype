23267f8c4809a153ac7b5abd97a14c84e6307d44
Naty-S <na7a5cha010@gmail.com>
2021-12-02 10:27:58 -0400

    clear las sesion in .logs

--- modules/node.py
+++ modules/node.py
@@ -8,22 +8,35 @@ import config.variables    as vars
 
 class Node:
 
-  def __init__(self, name: str, port: int, neighbours: list[int], logDir: str, bc: bc.Blockchain) -> None:
+  def __init__(self, name: str, port: int, neighbours: list[int], logDir: str, blockchain: bc.Blockchain) -> None:
 
     self.__name         = name
     self.__port         = port
     self.__logFile      = logDir + self.__name + ".log"
-    self.__neighbours   = neighbours # [port]
+    # [int]: Neighbours ports
+    self.__neighbours   = neighbours
+    # [dict]: Transactions already spread
+    self.__spreadTxs    = []  
+    # [dict]: Blocks already spread
+    self.__spreadBlocks = []  
+    # [tx.__dict__]
+    self.__mempool      = queue.Queue(-1)
+    # Block.__dict__
+    self.__minedBlock   = queue.Queue(1)
+    # Shared blockchain
+    self.__blockchain   = blockchain
+    # TODO: List used whe forks occurs
+    self.__myBlockchain = []
+    # [dict] TODO
+    self.__orphanBlocks = []
     self.__socket       = socket.socket()
-    self.__spreadTxs    = []  # [dict]
-    self.__spreadBlocks = []  # [dict]
-    self.__orphanBlocks = []  # [dict] TODO: forks
-    self.__mempool      = queue.Queue()  # [tx.__dict__]
-    self.__minedBlock   = queue.Queue(1) # Block.__dict__
-    self.__blockchain   = bc  # Blockchain
     self.__netListenerT = threading.Thread(name=self.__name+"-netListener", target=self.__netListener)
     self.__minerT       = threading.Thread(name=self.__name+"-miner", target=self.__miner)
     self.__minerAbort   = threading.Event()
+
+    # Clear last sesion
+    with open(self.__logFile, "w+") as file:
+      file.write(f"[{time.asctime()}]: Starting Node: {self.__name}...")
 
     self.__socket.bind((b'localhost', self.__port))
     self.__netListenerT.start()
@@ -37,64 +50,75 @@ class Node:
     self.__socket.listen(5)
 
     while True:
-      c, addr = self.__socket.accept()
-      msg     = ast.literal_eval(c.recv(8192).decode())
+      connex  = self.__socket.accept()[0]
+      msg     = ast.literal_eval(connex.recv(8192).decode())
       request = msg[0]
-      ackSi   = request.encode() + b" ACK: " + self.__name.encode() + b": Si"
-      ackNo   = request.encode() + b" ACK: " + self.__name.encode() + b": No"
-      print(f"request : {request}")
+      ackSi   = request.encode() + b"ACK: " + self.__name.encode() + b": Si"
+      ackNo   = request.encode() + b"ACK: " + self.__name.encode() + b": No"
+      print(f"{self.__name}: Got request : {request}...")
 
       if (request == "Transaccion Nueva") | (request == "Transaccion"):
+        
         tx = msg[1]
-        if tx in self.__spreadTxs: continue
+        
+        if tx in self.__spreadTxs:
+          continue
 
         txId = tx["txId"]
         if self.__validateTx(tx):
-          self.__mempool.put(tx)
+
+          print(f"{self.__name}: Adding {txId} to mempool...")
+          self.__mempool.put(tx, True)
+          
           self.__spread("Transaccion", tx)
           self.__writeLog("Transaccion >"+txId+"< recibida y aceptada: ["+time.asctime()+"]\n")
           self.__spreadTxs.append(tx)
-          c.send(ackSi)
-          c.close()
+          connex.send(ackSi)
+          connex.close()
         else:
+          print(f"{self.__name}: Transaction {txId} rejected...")
+
           self.__writeLog("Transaccion <"+txId+"> recibida y rechazada: ["+time.asctime()+"]\n")
-          c.send(ackNo)
-          c.close()
+          connex.send(ackNo)
+          connex.close()
       else: # Se asume request == 'Bloque'
         b = msg[1]
-        if b in self.__spreadBlocks: continue
+        
+        if b in self.__spreadBlocks:
+          continue
 
         if self.__validateBlock(b):
-          print(f"aborting minig...")
+          print(f"{self.__name}: Aborting minig...")
           self.__minerAbort.set()
           self.__updateMempool(b)
           self.__chain(b)
           self.__spread("Bloque", b)
-          self.__writeLog("Bloque >"+str(b["bId"])+"< recibido y aceptado: ["+time.asctime()+"]\n")
+          self.__writeLog("Bloque >"+b["bId"]+"< recibido y aceptado: ["+time.asctime()+"]\n")
           self.__spreadBlocks.append(b)
-          c.send(ackSi)
-          c.close()
+          connex.send(ackSi)
+          connex.close()
         else:
-          self.__writeLog("Bloque <"+str(b["bId"])+"> recibido y rechazado: ["+time.asctime()+"]\n")
-          c.send(ackNo)
-          c.close()
+          print(f"{self.__name}: Block {b['bId']} rejected...")
+
+          self.__writeLog("Bloque <"+b["bId"]+"> recibido y rechazado: ["+time.asctime()+"]\n")
+          connex.send(ackNo)
+          connex.close()
       
       # Verify if miner mined a block
       if self.__minedBlock.qsize() != 0:
-        print(f"block mined: {self.__name}...")
+        
         b = self.__minedBlock.get(True)
+        print(f"{self.__name}: Block mined: {b['bId']}...")
         self.__spread("Bloque", b)
-        self.__writeLog("Bloque >"+str(b["bId"])+"< minado y propagado: ["+time.asctime()+"]")
+        self.__writeLog("Bloque >"+b["bId"]+"< minado y propagado: ["+time.asctime()+"]")
         self.__spreadBlocks.append(b)
-        self.__minerAbort.clear() # Reset to false
-
-      # if ctrl + c : SystemExit()/ sys.exit()
-      # threading.Event()
+        # Miner can start again
+        self.__minerAbort.clear()
 
 
   def __validateTx(self, tx: dict) -> bool:
 
-    print(f"validateTx {tx['txId']}...")
+    print(f"{self.__name}: Validating Tx: {tx['txId']}...")
 
     try:
       tx["inputs"]
@@ -121,15 +145,17 @@ class Node:
 
   def __validateBlock(self, b: dict) -> bool:
 
-    print(f"validateBlock {b['bId']}...")
+    print(f"{self.__name}: Validating Block: {b['bId']}...")
 
     txsMerkle = pymerkle.MerkleTree()
-    for tx in b["transactions"]: txsMerkle.update(tx["txId"])
+    for tx in b["transactions"]:
+      txsMerkle.update(tx["txId"])
 
     try:
       b["prevBlock"] == self.__blockchain.getChain()[-1].bId
       txsMerkle.rootHash.decode() == b["merkleRoot"]
-      int(b["bId"], 16) < 2**(256 - (vars.DIFFICULTY / 100)) # Hash got correct pow
+      # Verify Proof of Work
+      int(b["bId"], 16) < 2**(256 - (vars.DIFFICULTY / 100))
       return True
     except:
       return False
@@ -137,42 +163,47 @@ class Node:
 
   def __chain(self, b: dict) -> None:
 
-    bBlock = block.Block(b["prevBlock"], b["merkleRoot"], b["transactions"])
-    bBlock.bId = b["bId"]
+    print(f"{self.__name}: Adding block to blockchain: {b['bId']}...")
+
+    bBlock           = block.Block(b["prevBlock"], b["merkleRoot"], b["transactions"])
+    bBlock.bId       = b["bId"]
     bBlock.timestamp = b["timestamp"]
     self.__blockchain.add(bBlock)
 
 
   def __spread(self, request: str, info: dict) -> None:
 
+    print(f"{self.__name}: Spreading...")
     msg = str((request, info)).encode()
 
     for neighbour in self.__neighbours:
-      print(f"{self.__name} spread: {list(info.items())[0]} to {neighbour}...")
+      
+      print(f"{self.__name}: Sending to {neighbour}: {list(info.items())[0]}...")
+      
       s = socket.socket()
       s.connect((b'localhost', neighbour))
       s.send(msg)
-      print(f"\t{self.__name} got ack : {s.recv(512).decode()}")
+      print(f"\t{self.__name}: Got ack : {s.recv(512).decode()}")
       s.close()
 
 
   # Filter mempool with transaction not in the winner block
   def __updateMempool(self, winner: dict) -> None:
 
-    print(f"updateMempool {self.__mempool.queue}...")
+    print(f"{self.__name}: Updating mempool: {self.__mempool.queue}...")
 
     txsNotInWinner = [ tx for tx in self.__mempool.queue if tx not in winner["transactions"] ]
     self.__mempool = queue.Queue()
-    for tx in txsNotInWinner: self.__mempool.put(tx)
+    for tx in txsNotInWinner: self.__mempool.put(tx, True)
 
-    print(f"mempool updated {self.__mempool.queue}...")
+    print(f"{self.__name}: mempool updated {self.__mempool.queue}...")
 
 
   def __miner(self) -> None:
 
     while True:
 
-      print(f"{self.__name} mining...")
+      print(f"{self.__name}: Started mining...")
       
       newBlock   = block.Block(self.__blockchain.getChain()[-1].bId, "-1", [])
       merkleTree = pymerkle.MerkleTree()
@@ -180,29 +211,37 @@ class Node:
       # Ask if have to abort mining
       while not self.__minerAbort.is_set():
 
+        print(f"{self.__name}: Checking txs in mempool...")
+
         tx   = self.__mempool.get(True)
         txId = tx["txId"]
+        print(f"{self.__name}: Adding tx {txId} to new block...")
         newBlock.transactions.append(tx)
         newBlock.size += len(txId) # Length of the tx id
+        print(f"{self.__name}: Adding tx {txId} to merkle tree...")
         merkleTree.update(txId.encode())
         self.__mempool.task_done()
 
         # Full block
         if newBlock.size == 512:
+  
           newBlock.timestamp  = time.asctime()
           newBlock.merkleRoot = merkleTree.rootHash.decode()
+          print(f"{self.__name}: Proof of work...")
           newBlock.pow()
           self.__minedBlock.put(newBlock.__dict__, True)
 
-      # Reset to false
+      # Reset to false, restart mining
+      print(f"{self.__name}: Restarting mining...")
       self.__minerAbort.clear()
       
       # Return transacctions to mempool
+      print(f"{self.__name}: Returning transaccionts to mempool...")
       for tx in newBlock.transactions:
-        self.__mempool.put(tx)
-      
+        self.__mempool.put(tx, True)
       
 
   def __writeLog(self, msg: str) -> None:
 
-    with open(self.__logFile, "a+") as logFile: logFile.write(msg)
+    print(f"{self.__name}: Writing in log file...")
+    with open(self.__logFile, "a") as logFile: logFile.write(msg)
--- src/genTransac.py
+++ src/genTransac.py
@@ -1,4 +1,4 @@
-import random, socket, time, pdb, os
+import random, socket, time, os
 
 
 import config.variables    as vars
@@ -10,89 +10,94 @@ def genTransactions(users: list[id.User], nodes: list[id.Node], logDir: str) -> 
 
   logFile = logDir + "genTransac.log"
 
+  # Clear last sesion
+  with open(logFile, "w+") as file:
+    file.write(f"[{time.asctime()}]: Starting to generate transacctions...")
+
   x = 0
   # while True:
-  while x < 5:
-    print(f"tx: {x}...")
+  while x < 9:
+    print(f"\ntx: {x}...")
     node     = random.choice(nodes)
     sender   = random.choice(users)
     receiver = random.choice(users)
     
-    if sender == receiver:
-      print("\tsender == receiver...\n")
-      continue
+    if sender == receiver: continue
     
     satoshis = random.randint(1, 10000000) * random.random() + 1
-    t        = __genTran(sender, receiver, satoshis)
-    print("sending tx...")
-    __sendTran(t, node)
+    t        = __genTx(sender, receiver, satoshis)
+    print(f"sending tx: {t.txId}...")
+    __sendTx(t, node)
 
     msg = "Transaccion Nueva <"+str(t.txId)+"> enviada: ["+time.asctime()+"]\n"
-    with open(logFile, "a+") as file:
+    with open(logFile, "a") as file:
       file.write(msg)
 
+    time.sleep(60 / vars.FREQUENCY)
     x += 1
   
   print("finish gen txs")
 
 
-def __genTran(sender: id.User, receiver: id.User, satoshis: int) -> tx.Transaction:
+def __genTx(sender: id.User, receiver: id.User, satoshis: int) -> tx.Transaction:
 
   inputs       = []
   total_amount = 0
   for utxo in sender.utxos:
-    print("checking utxos...")
+    print(f"checking utxo: {utxo.txId}, from: {sender.address}...")
+
     for out in utxo.outputs:
-      print("checking out...")
+      print(f"checking out: {out}...")
+
       # Checks if the output is from the sender
-      if out["address"] != sender.address:
-        print("\tout[address] != sender.address...\n")
-        continue
+      if out["address"] != sender.address: continue
 
       if (out["spent"] == False) & (total_amount < satoshis):
+        
         print("creating input...")
+
         out["spent"] = True # Se asume que la red acepta la transaccion
         input        = tx.TxInput(utxo.txId, out["index"], out["value"], sender.address).__dict__
         inputs.append(input)
         total_amount += out["value"]
   
-  if total_amount < satoshis:
-    print("\ttotal_amount < satoshis...\n")
-    # yield
+    if total_amount < satoshis:
+      print("\ttotal_amount < satoshis...\n")
+      # yield
 
   print("creating tx...")
   new_tx = tx.Transaction(sender, receiver, satoshis, inputs)
   
   if total_amount > satoshis:
+    
     print("creating change...")
+
     change = tx.TxOutput(total_amount - satoshis, sender.address, len(new_tx.outputs) + 1).__dict__
     new_tx.outputs.append(change)
     sender.utxos.append(new_tx)
   
-  print("adding tx to receiver...")
+  print(f"adding tx to receiver: {receiver.address}...")
   receiver.utxos.append(new_tx)
     
   return new_tx
 
 
-def __sendTran(t: tx.Transaction, node: id.Node) -> None:
+def __sendTx(t: tx.Transaction, node: id.Node) -> None:
   
   s      = socket.socket()
-  print("socket created...")
   txDict = t.__dict__
   msg    = str(("Transaccion Nueva", txDict)).encode()
 
-  s.connect((socket.gethostname(), node.port))
+  s.connect((b'localhost', node.port))
   print(f"conected to {node.port}...")
   s.send(msg)
-  print(f"msg sended ...")
+  print(f"msg sended to: {node.name}...")
 
-  print(f"waiting ack ...")
-  # pdb.set_trace()
+  print(f"waiting ack...")
   ack = s.recv(512).decode()
-  print(f"\n\n\t\tack : {ack}\n\n")
+  print(f"\tgen txs got: {ack}...")
 
-  # if ack == "Si":
+  # if ack[-1] == 'i':
   #   # TODO: marcar t como gastada
   #   pass
   
