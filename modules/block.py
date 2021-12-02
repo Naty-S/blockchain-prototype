c919cf86a56bf451971c030570ce61ccbb4d707a
Naty-S <na7a5cha010@gmail.com>
2021-12-02 09:13:48 -0400

    solve block header hash = None

--- modules/block.py
+++ modules/block.py
@@ -6,24 +6,27 @@ import config.variables    as vars
 
 class Block:
 
-  def __init__(self, bId: str, prevBlock: str, merkleRoot: str, transactions: list[str]) -> None:
+  def __init__(self, prevBlock: str, merkleRoot: str, transactions: list[str]) -> None:
 
-    self.bId          = bId # block header hash
     self.size         = 0
     self.nonce        = 0
     self.merkleRoot   = merkleRoot
     self.prevBlock    = prevBlock
     self.transactions = transactions
     self.timestamp    = ""
+    header   = str(self.prevBlock) + str(self.nonce) + self.timestamp + str(self.transactions)
+    self.bId = hashlib.sha256(hashlib.sha256(header.encode()).hexdigest().encode()).hexdigest()
     self.height       = -1 # TODO position in the chain, pueden tener la misma altura al competir(forks)
 
 
-  def __calcHash(self):
+  def calcHash(self):
     
-    self.bId = hashlib.sha256(hashlib.sha256(str(self.__dict__)).hexdigest().encode()).hexdigest()
+    header   = str(self.prevBlock) + str(self.nonce) + self.timestamp + str(self.transactions)
+    self.bId = hashlib.sha256(hashlib.sha256(header.encode()).hexdigest().encode()).hexdigest()
+    
 
   def pow(self):
 
     while int(self.bId, 16) > 2**(256 - (vars.DIFFICULTY/100)):
       self.nonce += 1
-      self.__calcHash()
+      self.calcHash()
